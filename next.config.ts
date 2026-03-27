ecf900a7dd9612de5afaaf6789ac50b2c2592b46
Angelo <65523959+artosien@users.noreply.github.com>
2026-03-27 15:00:58 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 ﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+ // output: "export",
   trailingSlash: true,
   
   // Turbopack config (Next.js 16+ default bundler)
